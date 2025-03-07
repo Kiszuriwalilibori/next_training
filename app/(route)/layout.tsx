e4947ebe670e2a58423cc979fd173276542660f0
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-03-07 09:53:31 +0100

    t7

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,10 +1,8 @@
-export const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <h1>Navbar</h1>
             {children}
         </div>
     );
-};
-
-export default Layout;
+}
