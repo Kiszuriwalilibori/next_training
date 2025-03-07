3022550d840a10c557eb7c5ef6a823a10e96817e
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-03-07 14:01:04 +0100

    xxx

---
+++ app/(route)/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Loading = () => {
+    return (
+        <div>
+            Loading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading
+            LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading
+            LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading Loading Loading
+            LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading LoadingLoading Loading Loading LoadingLoading LoadingLoading LoadingLoading Loading
+        </div>
+    );
+};
+
+export default Loading;
