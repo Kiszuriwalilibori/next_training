529824cede5d1a730c59aa16eb4de736b2bc6d0b
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-03-07 10:49:33 +0100

    comm

--- app/(route)/about/page.tsx
+++ app/(route)/about/page.tsx
@@ -1,3 +1,4 @@
 export default function About() {
+    throw new Error("Eroor testowy");
     return <h2>About</h2>;
 }
--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -5,7 +5,7 @@ import Hello from "../components/hello";
 export default function Home() {
     return (
         <div>
-            <h2 className="text-3xl">Welcome to Next.js</h2>
+            <h2 className="text-3xl">Welcome to Next.js -Home Page </h2>
             <Hello />
         </div>
     );
